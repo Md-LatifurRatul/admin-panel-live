f93e517a09d74b4c80bc4b9c92a13a9feb852769
Md-LatifurRatul <latifursoikot@gmail.com>
2025-07-10 20:52:36 +0600

    banner bug fixed

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,14 +22,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "4ae1572311e7368687238e90dfe064b0",
+"flutter_bootstrap.js": "0364bb6dd5ef60a57470239a5040d7f3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "e018cefa14d320b8de6331a4724fabea",
 "/": "e018cefa14d320b8de6331a4724fabea",
-"main.dart.js": "927f9113b2c80769be8a258ea800c019",
+"main.dart.js": "27e9ce833dd3e4c33a33e16beae905b5",
 "manifest.json": "dc046bc8c5f9bbd972d886b961af0909",
 "version.json": "12f2a0d3f82d82763aeebabebd5021ae"};
 // The application shell files that are downloaded before a service worker can
